f374876eb48f6f92cc665e3ae93888b71ce4f5e6
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-08-25 23:12:14 +0530

    feat: Add lazy loading for images in BlogCard component

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,29 +1,37 @@
 import { Bookmark } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 
 interface BlogCardProps {
-    title: string;
-    author: string;
-    content: string;
-    publishedDate: string;
-  }
-  
-  export const BlogCard = ({
-    title,
-    author,
-    content,
-    publishedDate,
-  }: BlogCardProps) => {
-    return (
-      <div className="container p-4 mx-auto">
-        <div className="max-w-4xl p-4 mx-auto bg-white rounded-lg shadow-md md:p-6 lg:p-8 dark:bg-gray-800">
-          <div className="flex">
-            <div className="flex flex-col justify-start">
-              <h2 className="mb-2 text-xl font-bold md:text-2xl lg:text-3xl">
-                {title}
-              </h2>
-              <p className="w-[80%] text-sm text-gray-700 md:text-base lg:text-lg dark:text-gray-300">
-                {content.slice(0, 100) + "..."}
-              </p>
+  title: string;
+  author: string;
+  content: string;
+  publishedDate: string;
+}
+
+export const BlogCard = ({
+  title,
+  author,
+  content,
+  publishedDate,
+}: BlogCardProps) => {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const toggleBookmark = () => {
+    setIsBookmarked(!isBookmarked);
+    toast(isBookmarked ? "Removed from bookmarks" : "Added to bookmarks");
+  };
+
+  return (
+    <div className="container p-4 mx-auto">
+      <div className="max-w-4xl p-4 mx-auto bg-white rounded-lg shadow-md md:p-6 lg:p-8 dark:bg-gray-800">
+        <div className="flex">
+          <div className="flex flex-col justify-start">
+            <h2 className="mb-2 text-xl font-bold md:text-2xl">{title}</h2>
+            <p className="w-[80%] text-sm text-gray-700 md:text-base lg:text-lg dark:text-gray-300">
+              {content.slice(0, 100) + "..."}
+            </p>
+            <div className="mt-4">
               <div className="flex items-center gap-2 mt-4 text-sm">
                 <p className="text-sm dark:text-muted-foreground text-muted-foreground">
                   By {author}
@@ -31,14 +39,27 @@ interface BlogCardProps {
                 <p className="text-sm dark:text-muted-foreground text-muted-foreground">
                   {publishedDate}
                 </p>
-                <Bookmark size={20} className="ml-auto mr-20 cursor-pointer text-muted-foreground" />
+
+                <Bookmark
+                  size={20}
+                  className={`ml-auto mr-20 cursor-pointer ${
+                    isBookmarked ? "fill-red-600" : "text-muted-foreground"
+                  }`}
+                  onClick={toggleBookmark}
+                />
               </div>
             </div>
-            <div className="ml-auto">
-              <img src="./images.jpeg" className="flex" width={200} height={200} />
-            </div>
+          </div>
+          <div className="ml-auto">
+            <img
+              src="./images.jpeg"
+              className="flex"
+              width={200}
+              height={200}
+            />
           </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
