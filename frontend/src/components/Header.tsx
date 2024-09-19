4e1c4fdf01b93ce3e7a5d37b96467cc9992e943b
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-09-19 23:49:46 +0530

    refactor: Update Header component layout and styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,13 +6,15 @@ export const Header = () => {
   return (
     <div>
       <div className="p-4 border-b-2 dark:border-b shadow-xl dark:bg-[#00091D] bg-white flex">
-        <ModeToggle />
-        <div className="flex items-center justify-end w-full gap-6">
-        <span className="flex items-center gap-2 cursor-pointer">
-            <SquarePen size={17}/>
+        <div className="mx-8">
+          <ModeToggle />
+        </div>
+        <div className="flex items-center justify-end w-full gap-6 mx-8">
+          <span className="flex items-center gap-2 cursor-pointer">
+            <SquarePen size={17} />
             Write
-        </span>
-        <Avatar />
+          </span>
+          <Avatar />
         </div>
       </div>
     </div>
