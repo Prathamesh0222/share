492d1312fb389ef43dbb12f386120a9ab4730d32
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-09-23 22:43:19 +0530

    refactor: Update package.json and App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import {
   BLOG_ID_URL,
   BLOG_URL,
+  HOME_URL,
+  PUBLISH_URL,
   SIGNIN_URL,
   SIGNUP_URL,
 } from "./constants/config";
@@ -13,20 +15,23 @@ import { Signin } from "./routes/Signin";
 import Blog from "./routes/Blog";
 import { DotBackground } from "./components/dotBackground";
 import BlogPost from "./routes/BlogPost";
+import Publish from "./routes/Publish";
 
 function App() {
   return (
     <>
       <DotBackground />
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <Toaster />
         <div>
           <BrowserRouter>
             <Routes>
+              <Route path={HOME_URL} element={<Signup />} />
               <Route path={SIGNUP_URL} element={<Signup />} />
               <Route path={SIGNIN_URL} element={<Signin />} />
               <Route path={BLOG_URL} element={<Blog />} />
               <Route path={BLOG_ID_URL} element={<BlogPost />} />
+              <Route path={PUBLISH_URL} element={<Publish />} />
             </Routes>
           </BrowserRouter>
         </div>
