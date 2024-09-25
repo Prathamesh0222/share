58210cf7dba928fd2ddaaf0733b24f4f4960fd6c
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2024-09-25 23:48:58 +0530

    refactor: Update BlogPost component to display a specific blog post

--- frontend/src/routes/BlogPost.tsx
+++ frontend/src/routes/BlogPost.tsx
@@ -1,11 +1,15 @@
 import { IdPost } from "@/components/IdPost";
 import { useBlog } from "@/hooks/useBlog";
+import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
+  const { id } = useParams();
   const { blogs, loading } = useBlog();
   if (loading) return <div>Loading...</div>;
   if (!blogs || blogs.length === 0) return <div>No blog posts available</div>;
-  return <IdPost blog={blogs[0]} />;
+  const blog = blogs.find((blog) => blog.id === id);
+  if (!blog) return <div>Blog not found</div>;
+  return <IdPost blog={blog} />;
 };
 
 export default BlogPost;
