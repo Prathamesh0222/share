ac20bedf73d36ce8c49843bc12c3958b05302d04
prathameshpimpalkar <prathameshpimpalkar2002@gmail.com>
2025-01-08 23:56:16 +0530

    feat: Integrate Header component into BookmarkedPosts and enhance Header layout with new icons and responsive design; update button styles for better user interaction

--- frontend/src/components/BookmarkedPosts.tsx
+++ frontend/src/components/BookmarkedPosts.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useBookmark } from "@/hooks/useBookmark";
 import { BlogCard } from "./BlogCard";
+import { Header } from "./Header";
 
 const BookmarkedPosts = () => {
   const { bookmarkedPosts, getBookmarks } = useBookmark();
@@ -10,25 +11,28 @@ const BookmarkedPosts = () => {
   }, []);
 
   return (
-    <section className="p-4 mx-auto bg-white dark:bg-slate-950 max-w-7xl sm:p-6 lg:p-8">
-      <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
-        Bookmarked Posts
-      </h2>
-      <div className="space-y-5">
-        {bookmarkedPosts.map((post) => (
-          <BlogCard
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            author={post.author}
-            content={post.content}
-            imgUrl={post.imgUrl}
-            tags={post.PostTag.map((postTag: any) => postTag.tag)}
-            published={post.published}
-          />
-        ))}
+    <div>
+      <Header />
+      <div className="p-4 mx-auto max-w-7xl sm:p-6 lg:p-8">
+        <h2 className="text-2xl font-bold text-gray-900 dark:text-white mb-4">
+          Bookmarked Posts
+        </h2>
+        <div className="space-y-5">
+          {bookmarkedPosts.map((post) => (
+            <BlogCard
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              author={post.author}
+              content={post.content}
+              imgUrl={post.imgUrl}
+              tags={post.PostTag.map((postTag: any) => postTag.tag)}
+              published={post.published}
+            />
+          ))}
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { SquarePen } from "lucide-react";
+import { BookmarkCheck, Signature, SquarePen } from "lucide-react";
 import { Avatar } from "./Avatar";
 import { ModeToggle } from "./mode-toggle";
 import { Link } from "react-router-dom";
@@ -17,27 +17,37 @@ export const Header = () => {
         type: "spring",
       }}
     >
-      <div className="p-4 border-b shadow-xl dark:bg-zinc-950 bg-white flex">
-        <div className="flex w-full items-center">
+      <div className="p-4 border-b shadow-xl dark:bg-zinc-950 bg-white">
+        <div className="max-w-7xl mx-auto flex items-center">
           <Link to={"/blog"}>
-            <h1 className="text-2xl font-bold cursor-pointer mx-2">BlogInk</h1>
+            <div className="flex gap-2 text-xl font-bold cursor-pointer mx-2">
+              <span className="mt-1">
+                <Signature />
+              </span>
+              BlogInk
+            </div>
           </Link>
-          <div className="mx-8">
+          <div className="mx-3">
             <ModeToggle />
           </div>
-        </div>
-        <div className="flex items-center justify-end w-full gap-6 mx-2">
-          <Link to={BOOKMARK_URL}>
-            <Button variant={"default"}>View Bookmarked Posts</Button>
-          </Link>
-          <Link to={"/publish"}>
-            {" "}
-            <span className="flex items-center gap-2 cursor-pointer hover:underline">
-              <SquarePen size={17} />
-              Write
-            </span>
-          </Link>
-          <Avatar />
+          <div className="flex items-center justify-end w-full gap-5 mx-2">
+            <Link to={BOOKMARK_URL}>
+              <Button variant={"outline"}>
+                <span className="md:hidden">
+                  <BookmarkCheck size={20} />
+                </span>
+                <span className="hidden md:block">View Bookmarked Posts</span>
+              </Button>
+            </Link>
+            <Link to={"/publish"}>
+              {" "}
+              <span className="flex items-center gap-2 cursor-pointer hover:underline">
+                <SquarePen size={17} />
+                Write
+              </span>
+            </Link>
+            <Avatar />
+          </div>
         </div>
       </div>
     </motion.div>
