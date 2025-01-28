0a84787952880e29042f5a0792368d8144162cde
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-01-28 23:53:20 +0530

    feat: Update environment configuration to use VITE_BACKEND_URL; add .env to .gitignore

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 export const SIGNUP_URL = "/signup";
 export const SIGNIN_URL = "/signin";
 export const HOME_URL = "/";
