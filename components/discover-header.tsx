04dee4c5afb85c01344039974a425a63100a4020
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-10-26 09:12:21 +0530

    changed font, global.css, and created header

---
+++ components/discover-header.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { TypeOutline } from "lucide-react";
+
+export const DiscoverHeader = () => {
+  const router = useRouter();
+  return (
+    <header className="flex justify-between p-4">
+      <TypeOutline className="text-green-500 size-7" />
+      <Button
+        className="rounded-xl font-bold cursor-pointer"
+        onClick={() => {
+          router.push("/signin");
+        }}
+      >
+        Get Started
+      </Button>
+    </header>
+  );
+};
