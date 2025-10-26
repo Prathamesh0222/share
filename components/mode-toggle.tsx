4824fade642828552b74c17e5733c4f16610a2d1
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-10-26 09:42:52 +0530

    Add theme toggle functionality with ModeToggle component and ThemeProvider

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,25 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+
+export const ModeToggle = () => {
+  const { theme, setTheme } = useTheme();
+
+  const updateTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button
+      variant={"ghost"}
+      className="cursor-pointer"
+      onClick={() => updateTheme()}
+    >
+      {theme === "dark" ? (
+        <Sun className="size-4 hover:text-green-500 text-green-500" />
+      ) : (
+        <Moon className="size-4 hover:text-green-500 text-green-500" />
+      )}
+    </Button>
+  );
+};
