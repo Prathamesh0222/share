8d4a17d147264d9550b3b9d0645e16e2ecab3bd9
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-10-27 10:39:08 +0530

    integrated dropdown menu in the header

--- components/discover-header.tsx
+++ components/discover-header.tsx
@@ -2,24 +2,75 @@
 
 import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
-import { TypeOutline } from "lucide-react";
+import { LogOut, SquarePen, TypeOutline, User } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
+import { signOut, useSession } from "@/lib/auth-client";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const DiscoverHeader = () => {
   const router = useRouter();
+  const { data } = useSession();
+
   return (
-    <header className="flex justify-between p-4">
+    <header className="flex justify-between p-4 max-w-7xl mx-auto">
       <TypeOutline className="text-green-500 size-7" />
       <div className="flex gap-4 items-center">
         <ModeToggle />
-        <Button
-          className="rounded-xl font-semibold cursor-pointer"
-          onClick={() => {
-            router.push("/signin");
-          }}
-        >
-          Get Started
-        </Button>
+        {data?.user ? (
+          <>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <div className="flex items-center justify-center w-10 h-10 rounded-full border bg-green-500 text-green-200 font-bold text-lg">
+                  {data.user.email
+                    ? data.user.email.charAt(0).toUpperCase()
+                    : "?"}
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel className="font-semibold">
+                  {data.user.name}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  onClick={() => {
+                    router.push("/publish");
+                  }}
+                >
+                  <SquarePen className="focus:text-green-500" />
+                  Write
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <User className="focus:text-green-500" />
+                  Profile
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => {
+                    signOut();
+                  }}
+                >
+                  <LogOut className="focus:text-green-500" />
+                  Logout
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </>
+        ) : (
+          <Button
+            className="rounded-xl font-semibold cursor-pointer"
+            onClick={() => {
+              router.push("/signin");
+            }}
+          >
+            Get Started
+          </Button>
+        )}
       </div>
     </header>
   );
