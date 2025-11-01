a09ffa348255e9da1419ab5f9d1e86cab2ce96ca
Prathamesh0222 <prathameshpimpalkar2002@gmail.com>
2025-11-01 23:57:08 +0530

    fixed domain error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [{ hostname: "res.cloudinary.com" }],
+    remotePatterns: [
+      { hostname: "res.cloudinary.com" },
+      { hostname: "lh3.googleusercontent.com" },
+    ],
   },
 };
 
